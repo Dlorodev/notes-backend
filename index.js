63f81d21e459c4f87e162ff8ed6112fa2262785e
Dlorodev <dlorodev@gmail.com>
2025-08-06 15:49:21 -0500

    uploading changes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const http = require('http');
+//PARTE 3C - USANDO LA BASE DE DATOS EN LOS CONTROLADORES DE RUTA
+//const http = require('http');
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const Note = require('./models/note.js')
 //const whiteList = ['http://localhost:5173']
-
-
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static('dist'));
 
-let notes = [
+/*let notes = [
     {
         id: 1,
         content: "HTML is easy",
@@ -50,10 +52,26 @@ app.get('/', (request, response) => {
 })
 //get all notes
 app.get('/api/notes', (request, response) => {
-    response.json(notes);
+    //response.json(notes);
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 //get one note
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id).then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => {
+        next(error)
+        /*
+        console.log(error)
+        response.status(400).send({ error: 'malformatted id' })*/
+    })
+    /*
     const id = Number(request.params.id);
     const note = notes.find(n => {
         //console.log(n.id, typeof n.id, id, typeof id, n.id === id)
@@ -67,13 +85,15 @@ app.get('/api/notes/:id', (request, response) => {
     }
     console.log(request.params)
     console.log(typeof (note));
+    */
 })
-//delete one note
+
+/*delete one note
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = notes.filter(n => n.id !== id)
+    //notes = notes.filter(n => n.id !== id)
     response.status(204).end()
-})
+})*/
 
 const generateId = () => {
     const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
@@ -81,7 +101,7 @@ const generateId = () => {
 }
 
 //add one note
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (!body.content) {
@@ -91,18 +111,65 @@ app.post('/api/notes', (request, response) => {
             }
         )
     }
-
-    const note = {
+    /*const note = {
         content: body.content,
         important: Boolean(body.important) || false,
         id: generateId()
-    }
+    }*/
 
-    notes = notes.concat(note)
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    })
+
+    //notes = notes.concat(note)
     //console.log(note)
     //console.log(request.headers)
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    }).catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+//update one note
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query' }).then(updatedNote => {
+        response.json(updatedNote)
+    }).catch(error => next(error))
+})
+
+//delete one note
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    }).catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(unknownEndpoint)
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
