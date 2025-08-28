f8d9b69636907a2b2f747ae30099b7feb2a4f26e
Dlorodev <dlorodev@gmail.com>
2025-08-28 17:55:10 -0500

    add the test database uri

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const config = require('./utils/config.js')
 const logger = require('./utils/logger.js')
 
 
-app.listen(config.PORT, () => logger.inf0(`Server running on port ${config.PORT}`))
+app.listen(config.PORT, () => logger.info(`Server running on port ${config.PORT}`))
